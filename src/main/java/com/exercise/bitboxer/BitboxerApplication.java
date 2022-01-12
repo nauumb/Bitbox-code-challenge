97778ccf4413b56d69258e060b857fdc32978918
NAUZET MARTÍN BÁEZ <nauzet.martin@ikeasi.com>
2022-01-12 15:59:20 +0000

    First commit

---
+++ src/main/java/com/exercise/bitboxer/BitboxerApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.bitboxer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitboxerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitboxerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/bitboxer/BitboxerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.bitboxer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitboxerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
